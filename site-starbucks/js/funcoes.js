80fb6af7024cd46917ef2c0a3df79001ec389ea7
João Vitor Polizelli <73860859+joaovpolizelli@users.noreply.github.com>
2021-01-25 20:09:36 -0300

    Add files via upload

---
+++ site-starbucks/js/funcoes.js
@@ -0,0 +1,10 @@
+function TrocarImagem(imagemNova){
+// document.querySelector("starbucks").src = imagemNova;
+let copo = document.querySelector(".mcdonalds");
+copo.src = imagemNova;
+}
+
+function MudarCirculo(corNova){
+let circulo = document.querySelector(".circulo");
+circulo.style.background = corNova;
+}
